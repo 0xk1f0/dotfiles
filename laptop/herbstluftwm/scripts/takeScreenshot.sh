20be05f52f135692c3a9343770482a5b88c6ca89
K1llf0rce <fteppan@gmx.at>
2021-09-28 23:51:52 +0200

    modify laptop polybar

--- laptop/herbstluftwm/scripts/takeScreenshot.sh
+++ laptop/herbstluftwm/scripts/takeScreenshot.sh
@@ -1,5 +1,17 @@
 #take screenshot and copy it to clipboard
-maim -s /tmp/screenshot.png && xclip -selection clipboard -t image/png -i /tmp/screenshot.png
+maim -su /tmp/screenshot.png && xclip -selection clipboard -t image/png -i /tmp/screenshot.png
 
-#notify user
+exitCode=$(echo $?)
+
+#notify user depending on exit code
+if [ "$exitCode" -eq 0 ]; then
+
+#taken screenshot, inform user
 notify-send "Screenshot Captured" "Copied to Clipboard!" -h string:x-canonical-private-synchronous:screenshot
+
+else
+
+#exitcode is non zero, screenshot aborted, alert user
+notify-send "Screenshot Aborted" "Nothing Captured!" -h string:x-canonical-private-synchronous:screenshot
+
+fi
