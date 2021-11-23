7bc92ef7f4ef77a848574378f6573281de061dc3
K1llf0rce <fteppan@gmx.at>
2021-11-23 21:14:09 +0100

    pc configs and teams blur fix

--- pc/qtile/autostart.sh
+++ pc/qtile/autostart.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/bash
 
+# dunst
+killall -q dunst
+dunst --startup_notification &
+
 # picom
+killall -q picom
 picom --experimental-backends &
 if [ "$(echo $?)" -eq 0 ]; then
     notify-send "Picom Success" "Started Successfully!" -h string:x-canonical-private-synchronous:picom
+fi
+
+# nitrogen
+nitrogen --restore &
+if [ "$(echo $?)" -eq 0 ]; then
+    notify-send "Nitrogen Success" "Wallpaper Resored!" -h string:x-canonical-private-synchronous:nitrogen
 fi
 
 # numlock
--- pc/qtile/config.py
+++ pc/qtile/config.py
@@ -38,18 +38,14 @@ bordersize=2
 # key binds
 keys = [
     # move windows
-    Key([mod], "Left", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "Right", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "Down", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "Up", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "Down", lazy.group.next_window(), desc="Move focus down"),
+    Key([mod], "Up", lazy.group.prev_window(), desc="Move focus up"),
     Key([mod, "shift"], "Left", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "Right", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "Down", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "Up", lazy.layout.shuffle_up(), desc="Move window up"),
     Key([mod, "mod1"], "Down", lazy.layout.flip_down(), desc="Flip Layout downwards"),
     Key([mod, "mod1"], "Up", lazy.layout.flip_up(), desc="Flip Layout upwards"),
-    Key([mod, "mod1"], "Left", lazy.layout.flip_left(), desc="Flip Layout leftside"),
-    Key([mod, "mod1"], "Right", lazy.layout.flip_right(), desc="Flip Layout rightside"),
     Key([mod, "control"], "Left", lazy.layout.grow_left(), desc="Grow window to the left"),
     Key([mod, "control"], "Right", lazy.layout.grow_right(), desc="Grow window to the right"),
     Key([mod, "control"], "Down", lazy.layout.grow_down(), desc="Grow window down"),
@@ -59,8 +55,8 @@ keys = [
     # modify windows / layout
     Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen"),
     Key([mod], "s", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "space", lazy.next_screen(), desc="Toggle between screens"),
-    Key([mod], "Tab", lazy.next_screen(), desc="Toggle between screens"),
+    Key([mod], "Right", lazy.next_screen(), desc="Toggle between screens"),
+    Key([mod], "Left", lazy.prev_screen(), desc="Toggle between screens"),
     Key([mod, "shift"], "c", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "shift"], "r", lazy.restart(), desc="Restart Qtile"),
     Key([mod, "shift"], "q", lazy.spawn(f"{scriptPath}rofipower.sh"), desc="Power Menu"),
@@ -296,11 +292,6 @@ reconfigure_screens = True
 auto_minimize = True
 wmname = "LG3D"
 
-# start thingies ONCE
-@hook.subscribe.startup_once
-def start_once():
-    subprocess.call([home + '/.config/qtile/scripts/xrandrapply.sh'])
-    subprocess.call([home + '/.config/qtile/autostart.sh'])
-
-# start other thingies on startup AND reload
+# autostart
+subprocess.call([home + '/.config/qtile/scripts/xrandrapply.sh'])
 subprocess.call([home + '/.config/qtile/autostartReload.sh'])
