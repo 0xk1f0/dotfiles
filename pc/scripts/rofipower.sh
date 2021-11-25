2ad72e98953f1e0b0838ed43525f485b295aadcc
K1llf0rce <fteppan@gmx.at>
2021-11-25 19:29:03 +0100

    few updates

--- pc/scripts/rofipower.sh
+++ pc/scripts/rofipower.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rofi -show power-menu -modi 'power-menu:~/.config/qtile/scripts/rofi-power --no-symbols --choices=shutdown/reboot/logout'
+rofi -show power-menu -modi 'power-menu:~/.config/scripts/rofi-power --no-symbols --choices=shutdown/reboot/logout'
