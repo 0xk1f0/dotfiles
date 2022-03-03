eb44a8a25c39068703eb09b82c1a913adbb0f00b
K1llf0rce <fteppan@protonmail.com>
2022-03-03 21:41:07 +0100

    update push script

--- pushAll.sh
+++ pushAll.sh
@@ -1,31 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-#Universal git push script
+# Universal git push script
 
-#update to newest
 echo "Checking for up to date local repo..."
 git pull
-sleep 2
-echo "done"
-
-#add all
-clear
-echo "Adding all files"
+sleep 1
 git add .
-echo "done"
-
-#commit with message
-clear
-read -p "Specify a commit message: " commitMsg
+read -ep "Specify a commit message: " commitMsg
 git commit -m "$commitMsg"
-sleep 2
-
-#push
+sleep 1
 echo "Pushing commit..."
+sleep 1
 git push
-echo "done"
-
-#exit
-clear
 echo "Exiting..."
 exit 0
