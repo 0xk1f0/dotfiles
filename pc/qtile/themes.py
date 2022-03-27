c50cbc72b310fefa7eb1a46c97166ecf7d80ef7a
K1llf0rce <fteppan@protonmail.com>
2022-03-27 23:39:31 +0200

    update pc configs

--- pc/qtile/themes.py
+++ pc/qtile/themes.py
@@ -5,74 +5,12 @@
 # \__, /\__/_/_/\___/     /_/|_/_/_/  \____/
 #   /_/
 
-# neon/green
-"""
 grey = dict(
-	accentNormal="#204038",
+	accentNormal="#a06058",
 	accentUrgent="ff0000",
-	accentActive="#70e870",
-	accentForeground="#70e870",
-	accentBackground="#182828",
-	accentModuleBackground="#182828"
-)
-"""
-
-# neon/pink
-"""
-grey = dict(
-	accentNormal="#784068",
-	accentUrgent="ff0000",
-	accentActive="#e890b0",
-	accentForeground="#e890b0",
-	accentBackground="#201828",
-	accentModuleBackground="#201828"
-)
-"""
-
-# greyish
-"""
-grey = dict(
-	accentNormal="#786868",
-	accentUrgent="ff0000",
-	accentActive="#f0e8d0",
-	accentForeground="#887880",
-	accentBackground="#101010",
-	accentModuleBackground="#101010"
-)
-"""
-
-# bluish
-"""
-grey = dict(
-	accentNormal="#3060a0",
-	accentUrgent="ff0000",
-	accentActive="#68b8e8",
-	accentForeground="#98a8c8",
-	accentBackground="#002038",
-	accentModuleBackground="#002038"
-)
-"""
-
-# dark/light mix bluish
-"""
-grey = dict(
-	accentNormal="#305080",
-	accentUrgent="ff0000",
-	accentActive="#50a8c8",
-	accentForeground="#d0e0e8",
-	accentBackground="#101018",
-	accentModuleBackground="#101018"
-)
-"""
-
-# bluish/grey/green something
-
-grey = dict(
-	accentNormal="#9090b8",
-	accentUrgent="ff0000",
-	accentActive="#4878b0",
-	accentForeground="#b8c0f0",
-	accentBackground="#182028",
-	accentModuleBackground="#182028"
+	accentActive="#f89068",
+	accentForeground="#986808",
+	accentBackground="#081820",
+	accentModuleBackground="#081820"
 )
 
