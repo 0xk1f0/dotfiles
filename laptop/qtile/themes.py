c2a7c89b2bd24e363c0b2b7d42f922f2c53e5c5f
K1llf0rce <fteppan@protonmail.com>
2022-03-28 00:25:29 +0200

    update laptop configs

--- laptop/qtile/themes.py
+++ laptop/qtile/themes.py
@@ -5,86 +5,11 @@
 # \__, /\__/_/_/\___/     /_/|_/_/_/  \____/
 #   /_/
 
-# neon/green
-"""
 grey = dict(
-	accentNormal="#204038",
+	accentNormal="#505048",
 	accentUrgent="ff0000",
-	accentActive="#70e870",
-	accentForeground="#70e870",
-	accentBackground="#182828",
-	accentModuleBackground="#182828"
-)
-"""
-
-# neon/pink
-"""
-grey = dict(
-	accentNormal="#784068",
-	accentUrgent="ff0000",
-	accentActive="#e890b0",
-	accentForeground="#e890b0",
-	accentBackground="#201828",
-	accentModuleBackground="#201828"
-)
-"""
-
-# greyish
-"""
-grey = dict(
-	accentNormal="#786868",
-	accentUrgent="ff0000",
-	accentActive="#f0e8d0",
-	accentForeground="#887880",
-	accentBackground="#101010",
-	accentModuleBackground="#101010"
-)
-"""
-
-# bluish
-"""
-grey = dict(
-	accentNormal="#3060a0",
-	accentUrgent="ff0000",
-	accentActive="#68b8e8",
-	accentForeground="#98a8c8",
-	accentBackground="#002038",
-	accentModuleBackground="#002038"
-)
-"""
-
-# warm red
-"""
-grey = dict(
-	accentNormal="#703028",
-	accentUrgent="ff0000",
-	accentActive="#e8a8a0",
-	accentForeground="#c85048",
-	accentBackground="#282028",
-	accentModuleBackground="#282028"
-)
-"""
-
-# light red/pink
-
-"""
-grey = dict(
-	accentNormal="#b85868",
-	accentUrgent="ff0000",
-	accentActive="#e0e8e8",
-	accentForeground="#e83850",
-	accentBackground="#302028",
-	accentModuleBackground="#302028"
-)
-"""
-
-#orange/bluish/greish
-
-grey = dict(
-	accentNormal="#986880",
-	accentUrgent="ff0000",
-	accentActive="#e89098",
-	accentForeground="#d8d0d8",
-	accentBackground="#101028",
-	accentModuleBackground="#101028"
+	accentActive="#d8c0e0",
+	accentForeground="#805868",
+	accentBackground="#181818",
+	accentModuleBackground="#181818"
 )
