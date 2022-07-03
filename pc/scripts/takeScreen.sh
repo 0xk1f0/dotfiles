84b5e900ae3eaac97ee604534d4aea286b3df389
K1llf0rce <fteppan@protonmail.com>
2022-07-03 23:54:12 +0200

    adjust scripts

--- pc/scripts/takeScreen.sh
+++ pc/scripts/takeScreen.sh
@@ -2,18 +2,7 @@
 
 TIMESTAMP=$(date +%H_%M_%S)
 
-#take screenshot and copy it to clipboard
-if [ $1 == "sel" ]; then
-    maim -su /tmp/screenshot_$TIMESTAMP.png && xclip -selection clipboard -t image/png -i /tmp/screenshot_$TIMESTAMP.png
-else
-    x=3
-    while [ $x -gt 0 ]; do
-    dunstify -h string:x-dunst-stack-tag:screenshot "Taking Screenshot in..." "$x"
-    x=$[$x-1]
-    sleep 1
-    done
-    maim -u /tmp/screenshot_$TIMESTAMP.png && xclip -selection clipboard -t image/png -i /tmp/screenshot_$TIMESTAMP.png
-fi
+scrot -s -f -F /tmp/screenshot_$TIMESTAMP.png && xclip -selection clipboard -t image/png -i /tmp/screenshot_$TIMESTAMP.png
 
 exitCode=$(echo $?)
 
