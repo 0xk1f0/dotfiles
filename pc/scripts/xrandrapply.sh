c44b125c4f084a0c79341775b361f882ea9b8ecb
K1llf0rce <fteppan@protonmail.com>
2022-08-10 17:16:52 +0200

    adj for new monitor

--- pc/scripts/xrandrapply.sh
+++ pc/scripts/xrandrapply.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-xrandr --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-0 --primary --mode 2560x1440 --pos 1080x152 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate left
+xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 1080x144 --rotate normal --output DisplayPort-1 --mode 1920x1080 --pos 0x0 --rotate left --output DisplayPort-2 --off --output HDMI-A-0 --off
 xrandr --output DisplayPort-0 --mode 2560x1440 --rate 165
-xrandr --output HDMI-A-0 --set TearFree on
+xrandr --output DisplayPort-1 --mode 1920x1080 --rate 144
 xrandr --output DisplayPort-0 --set TearFree off
+xrandr --output DisplayPort-1 --set TearFree on
