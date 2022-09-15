b7f7758bf37bd02a5e1cbb81ad00f17475cd0aa6
K1llf0rce <fteppan@protonmail.com>
2022-09-15 22:23:25 +0200

    waybar updates

--- configs/pc/scripts/swayidle.sh
+++ configs/pc/scripts/swayidle.sh
@@ -3,6 +3,4 @@
 LOCKER="/home/$USER/.config/scripts/swaylock.sh"
 
 swayidle -w \
-timeout 600 'hyprctl dispatch dpms off' \
-timeout 720 $LOCKER \
-resume 'hyprctl dispatch dpms on' \
+timeout 600 $LOCKER \
