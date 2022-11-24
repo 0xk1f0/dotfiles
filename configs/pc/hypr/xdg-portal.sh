69bef18fd0842561109174b825e14cbd316db960
K1llf0rce <fteppan@protonmail.com>
2022-11-24 23:54:25 +0100

    fix some stuff

--- configs/pc/hypr/xdg-portal.sh
+++ configs/pc/hypr/xdg-portal.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 sleep 2
-systemctl stop --user xdg-desktop-portal
-systemctl restart --user xdg-desktop-portal-wlr
-systemctl restart --user xdg-desktop-portal-gtk
+systemctl stop --user \
+xdg-desktop-portal \
+xdg-desktop-portal-wlr \
+xdg-desktop-portal-gtk
+systemctl start --user xdg-desktop-portal-wlr
 sleep 2
 systemctl start --user xdg-desktop-portal
