0423f07d86c2e6bd51662cdce9706ac1820e0609
0xk1f0 <fteppan@protonmail.com>
2023-01-09 19:59:05 +0100

    readd brightness script

---
+++ configs/dotconfig/scripts/brightness.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# bash strict
+set -uo pipefail
+
+case $1 in
+    up)
+        # up Volume
+        brightnessctl s 5+
+        STATUS="$(brightnessctl g)"
+        ICON="xfpm-brightness-lcd"
+        ;;
+    down)
+        # down Volume
+        brightnessctl s 5-
+        STATUS="$(brightnessctl g)"
+        ICON="xfpm-brightness-lcd"
+        ;;
+esac
+
+# alert user
+dunstify \
+-a "chgBrt" \
+-r 55177 \
+-u low \
+-i "$ICON" "Brightness:" "$STATUS" \
+-h int:value:$((100*$STATUS/255))
