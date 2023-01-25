a92a385fbfeff7974d4ca9e520fc0f48501cd3b7
0xk1f0 <fteppan@protonmail.com>
2023-01-26 00:37:09 +0100

    fix theme script and correct xdg start

--- configs/dotconfig/hypr/scripts/xdg-portal.sh
+++ configs/dotconfig/hypr/scripts/xdg-portal.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 systemctl stop --user xdg-desktop-portal*
+systemctl stop --user xsettingsd
 systemctl start --user xdg-desktop-portal-wlr
 sleep 1
-systemctl start --user xdg-desktop-portal-wlr
+systemctl start --user xdg-desktop-portal-gtk
+systemctl start --user xsettingsd
 sleep 1
 systemctl start --user xdg-desktop-portal
