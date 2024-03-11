d69a866d237f6ca1b1ebad59b2b5634569173b95
0xk1f0 <dev@k1f0.dev>
2024-03-11 23:49:00 +0100

    clean up media.sh

--- configs/dotconfig/scripts/media.sh
+++ configs/dotconfig/scripts/media.sh
@@ -7,19 +7,13 @@ case $1 in
     prev)
         # play prev
         playerctl previous
-        STATUS="Previous"
-        ICON="media-skip-backward"
         ;;
     next)
         # play next
         playerctl next
-        STATUS="Next"
-        ICON="media-skip-forward"
         ;;
     pp)
         # playPause media
-        playerctl play-pause && STATUS=
-        STATUS="$(playerctl status)"
-        ICON="media-play"
+        playerctl play-pause
         ;;
 esac
