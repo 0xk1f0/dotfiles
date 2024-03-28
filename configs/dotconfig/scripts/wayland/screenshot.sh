52907504c3ed75e67a670240bc6809fd9f671f30
0xk1f0 <dev@k1f0.dev>
2024-03-28 03:19:32 +0100

    avoid starting multiple screenshot instances

--- configs/dotconfig/scripts/wayland/screenshot.sh
+++ configs/dotconfig/scripts/wayland/screenshot.sh
@@ -3,20 +3,21 @@
 # bash strict
 set -uo pipefail
 
-TIMESTAMP=$(date +%H_%M_%S)
-
-grim -g "$(slurp -d -w 0 -b '#000000cc' -c '#ffffffff' -B '#ffffffff')" - | wl-copy
-
-#notify user depending on exit code
-if [ "$?" -eq 0 ]; then
-    #taken screenshot, inform user
-    SCREEN_STATUS="Copied to Clipboard!"
-else
-    #exitcode is non zero, screenshot aborted
-    SCREEN_STATUS="Nothing Captured!"
+# check for running instance
+if not pgrep -x slurp && not pgrep -x grim; then
+    # take screenshot
+    grim -g "$(slurp -d -w 0 -b '#000000cc' -c '#ffffffff' -B '#ffffffff')" - | wl-copy
+    #notify user depending on exit code
+    if [ "$?" -eq 0 ]; then
+        #taken screenshot, inform user
+        SCREEN_STATUS="Copied to Clipboard!"
+    else
+        #exitcode is non zero, screenshot aborted
+        SCREEN_STATUS="Nothing Captured!"
+    fi
+    dunstify \
+    -a "tkScr" \
+    -r 44188 \
+    -u low \
+    -i "window_fullscreen" "Screenshot:" "$SCREEN_STATUS"
 fi
-dunstify \
--a "tkScr" \
--r 44188 \
--u low \
--i "window_fullscreen" "Screenshot:" "$SCREEN_STATUS"
