70aac57696eb6ce328b3352ba9351bb8c73c0f54
0xk1f0 <dev@k1f0.dev>
2024-05-05 00:29:16 +0200

    fix brightness script and lockscreen bat percent

--- configs/dotconfig/scripts/brightness.sh
+++ configs/dotconfig/scripts/brightness.sh
@@ -6,22 +6,23 @@ set -uo pipefail
 case $1 in
     up)
         # up Volume
-        brightnessctl s 5+
-        STATUS="$(brightnessctl g)"
+        brightnessctl s 5%+
         ICON="xfpm-brightness-lcd"
         ;;
     down)
         # down Volume
-        brightnessctl s 5-
-        STATUS="$(brightnessctl g)"
+        brightnessctl s 5%-
         ICON="xfpm-brightness-lcd"
         ;;
 esac
+
+# calc percent value
+PERCENT_BRIGHTNESS=$(brightnessctl i | grep "Current" | cut -d "(" -f 2 | tr -d ")")
 
 # alert user
 dunstify \
 -a "chgBrt" \
 -r 55177 \
 -u low \
--i "$ICON" "Brightness:" "$STATUS" \
--h int:value:$((100*$STATUS/255))
+-i "$ICON" "Brightness:" "$PERCENT_BRIGHTNESS" \
+-h int:value:$PERCENT_BRIGHTNESS
