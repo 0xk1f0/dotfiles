64bfb0786b903e759405e441414eededfd2caece
0xk1f0 <dev@k1f0.dev>
2025-07-01 21:49:53 +0200

    adjust hyprland config

--- configs/dotconfig/hypr/hyprinit.sh
+++ configs/dotconfig/hypr/hyprinit.sh
@@ -26,4 +26,4 @@ hypridle &
 
 # wallpaper
 pidof rwpspread && killall -s 15 -q rwpspread
-rwpspread -b hyprpaper -l hyprlock --monitors DP-1:27 DP-2:24 --experimental-ppi --bezel 100 -wpdi "/home/$USER/.wallpaper" &
+rwpspread -b hyprpaper -l hyprlock --monitors DP-1:27 DP-2:27 --experimental-ppi --bezel 80 -wpdi "/home/$USER/.wallpaper" &
